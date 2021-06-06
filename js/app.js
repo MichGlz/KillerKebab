14a22d6427277e5a13101f9479b401053e8edcd3
MichGlz <migu0403@stud.kea.dk>
2021-06-06 09:21:25 +0200

    first cometre with old files

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// document.querySelector(".letra").addEventListener("animationend", () => {
+//   document.querySelector("#sparkling").classList.remove("letra");
+//   document.querySelector("#sparkling").classList.add("letraB");
+// });
+window.addEventListener("load", consoleLog);
+function consoleLog() {
+  console.log("hola mundo");
+}
